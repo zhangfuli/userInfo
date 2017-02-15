45c3cef08577dc90bd212b1a93cdcdc864bb0f61
DESKTOP-8RMKUQ8\11710 <1171067930@qq.com>
2017-02-15 09:26:20 +0800

    实现路由

--- routes.js
+++ routes.js
@@ -4,7 +4,43 @@ module.exports = function (app) {
 	var users = require('.controllers/users_controllers');
 	app.use('/static' , express.static('./static'))
 			.use('/lib',express.static('../lib'));
+	app.get('/',function (req,res){
+	if(req.session.user){
+		res.render('index',{ username : req.session.username ,
+							 msg : req.session.msg});
+	}else{
+		req.session.msg = 'Access denied!';
+		res.redirect('/login');
+	}
+	});
+	app.get('/user' , function (req , res){
+		if(req.session.user){
+			res.render('user' ,{msg:req.session.msg});
+		}else{
+			req.session.msg = 'Access denied!';
+			res.redirect('/login');
+		}
+	});
+	app.get('/signup', function (req ,res){
+		if(req.session.user){
+			res.redirect('/');
+		}
+		res.render('signup' , {msg : req.session.msg});
+	});
+	app.get('/login' , function (req ,res){
+		if(req.session.user){
+			res.redirect('/');
+		}
+		res.render('login' ,{msg : req.session.msg});
+	});
+	app.get('/logout' , function (req ,res){
+		req.session.destory(function () {
+			res.redirect('/login');
+		});
+	});
+	app.post('/signup' ,users.signup);
+	app.post('/user/update' , users.updateUser);
+	app.post('/user/delete' , users.deleteUser);
+	app.post('/login' , users.login);
+	app.get('/user.profile' ,users.getUserProfile);
 };
-app.get('/',function (req,res){
-	
-})
